380d88cd5b10cf09b75624e3cbeb6239d9cf1663
Peter Czimmermann <peter.czimmermann@sensorup.com>
2022-08-24 22:35:37 -0700

    test: sdk upgrade only

--- src/__tests__/DynamoDBDataSourceWithClient.spec.ts
+++ src/__tests__/DynamoDBDataSourceWithClient.spec.ts
@@ -1,9 +1,11 @@
 import { DataSourceConfig } from 'apollo-datasource';
 
 import { DynamoDBClient, CreateTableCommandInput } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, DeleteCommand } from '@aws-sdk/lib-dynamodb';
 
 import { DynamoDBDataSource } from '../DynamoDBDataSource';
+
+const { MOCK_DYNAMODB_ENDPOINT } = process.env;
 
 interface TestItem {
   id: string;
@@ -28,35 +30,40 @@ const keySchema: CreateTableCommandInput['KeySchema'] = [
   },
 ];
 
-// const dynamodbMock = mockClient(DynamoDBClient)
-const dynamoDbClient = new DynamoDBClient({});
+const dynamoDbClient: DynamoDBClient = new DynamoDBClient({
+  ...(MOCK_DYNAMODB_ENDPOINT && {
+    endpoint: MOCK_DYNAMODB_ENDPOINT,
+    sslEnabled: false,
+    region: 'local',
+  }),
+});
 const client = DynamoDBDocumentClient.from(dynamoDbClient);
 
 const testWithClient = new TestWithClient('test_with_client', keySchema, client);
 testWithClient.initialize({ context: {}, cache: null });
 
-// const testItem: TestItem = {
-// id: 'testWithClientId',
-// item1: 'testing1',
-// item2: 'testing2',
-// };
+const testItem: TestItem = {
+  id: 'testWithClientId',
+  item1: 'testing1',
+  item2: 'testing2',
+};
 
 beforeAll(async () => {
-  // await testWithClient.dynamoDbDocClient.send(
-  //  new PutCommand({
-  //    TableName: testWithClient.tableName,
-  //    Item: testItem,
-  //  })
-  // );
+  await testWithClient.dynamoDbDocClient.send(
+    new PutCommand({
+      TableName: testWithClient.tableName,
+      Item: testItem,
+    })
+  );
 });
 
 afterAll(async () => {
-  // await testWithClient.dynamoDbDocClient.send(
-  //  new DeleteCommand({
-  //    TableName: testWithClient.tableName,
-  //    Key: { id: 'testWithClientId' },
-  //  })
-  // );
+  await testWithClient.dynamoDbDocClient.send(
+    new DeleteCommand({
+      TableName: testWithClient.tableName,
+      Key: { id: 'testWithClientId' },
+    })
+  );
 });
 
 describe('DynamoDBDataSource With Initialized Client', () => {
